c3fc982c1ffbce560d9eba4cac082d18de7b3f98
Jared Elliott <bootcamp@jaredclt.com>
2024-01-27 13:14:05 -0500

    Implemented get all & get by id functionality.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,19 +2,46 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    //Finding all categories and including the product module
+    const catSQL = await Category.findAll({
+      include: [{
+        model: Product,
+        required: false, //Left Join, so we get a category even if there is no product.
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }]
+    });
+    res.status(200).json(catSQL);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const prodSQL = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+        required: false,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }]
+    });
+    res.status(200).json(prodSQL);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+
+  /*
+    const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
+    console.log("Jane's auto-generated ID:", jane.id);
+  */
 });
 
 router.put('/:id', (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const prodSQL = await Product.findAll({
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      include: [{
+        model: Category,
+        required: true,
+      }]
+    });
+    res.status(200).json(prodSQL);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const prodSQL = await Product.findByPk(req.params.id, {
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      include: [{
+        model: Category,
+        required: true,
+      }]
+    });
+    res.status(200).json(prodSQL);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
